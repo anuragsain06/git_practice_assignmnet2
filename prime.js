4c17b25cee77e8c3a81644583b011776a1a0e6a1
Mrkishansharma <kishansharma6377@gmail.com>
2022-12-08 19:49:10 +0530

    file update

--- prime.js
+++ prime.js
@@ -1,1 +1,12 @@
-console.log(Hello World);
+let num = 13
+let factor = 0
+for(let i=1; i<=num; i++){
+    if(num%i==0){
+        factor++
+    }
+}
+if(factor==2){
+    console.log("true")
+}else{
+    console.log("false")
+}
